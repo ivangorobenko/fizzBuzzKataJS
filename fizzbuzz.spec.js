c9efe1c3c453510ce347d7d5104d66260ac21a08
Anthony Guérot <anthony@digikare.com>
2021-06-24 15:50:30 +0200

    feat: multiple de 15 et cas 0

--- fizzbuzz.spec.js
+++ fizzbuzz.spec.js
@@ -15,13 +15,24 @@ describe('Fizzbuzz ', () => {
         });
     });
 
+    [15, 30].map(v => {
+        it(`${v} (multiple de 5 et de 3) retourne FizzBuzz`, () => {
+            fizzbuzz(v).should.be.equals('FizzBuzz');
+        });
+    });
+    
     it('tous les autres cas renvoie la même valeur en string', () => {
         fizzbuzz(1).should.be.equals('1');
     });
 
+    it('0 renvoie la même valeur en string', () => {
+        fizzbuzz(0).should.be.equals('0');
+    })
 });
 
 const fizzbuzz = (value) => {
+    if(value === 0) return value.toString();
+    if (value % 15 === 0) return 'FizzBuzz';
     if (value % 3 === 0) return 'Fizz';
     if (value % 5 === 0) return 'Buzz';
     return value.toString();
