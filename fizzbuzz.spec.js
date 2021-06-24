00581bac51c81456cfb94e1494a867d2cbbfefba
Anthony Guérot <anthony@digikare.com>
2021-06-24 15:14:43 +0200

    init test fn

---
+++ fizzbuzz.spec.js
@@ -0,0 +1,10 @@
+describe('Fizzbuzz ', () => {
+    it('Un multiple de 3 retourne un Fizz', () => {
+        const res = fizzbuzz(3);
+        res.should.be.equals('Fizz');
+    });
+});
+
+function fizzbuzz() {
+
+}
