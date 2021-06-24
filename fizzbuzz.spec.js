dfeeac27bddb870c34f36b5ca1fc60bda454275a
Anthony Guérot <anthony@digikare.com>
2021-06-24 15:31:42 +0200

    feat: test Buzz multiple 5

--- fizzbuzz.spec.js
+++ fizzbuzz.spec.js
@@ -6,6 +6,17 @@ describe('Fizzbuzz ', () => {
     it('Un multiple de 3 retourne un Fizz', () => {
         fizzbuzz(3).should.be.equals('Fizz');
     });
+
+    [5, 10].map(v => {
+        it(`${v} (multiple de 5) retourne un Buzz`, () => {
+            fizzbuzz(v).should.be.equals('Buzz');
+        });
+    });
+
 });
 
-const fizzbuzz = () => "Fizz";
+const fizzbuzz = (value) => {
+    if (value % 5 === 0) return 'Buzz';
+
+    return 'Fizz';
+};
